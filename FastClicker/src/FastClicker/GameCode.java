f90fe2eda4aa7ef032a3b405fc00347d9bd24ad7
BT_1N3_25 <BT_1N3_25@BT_1N3_25.bktech.local>
2018-01-03 12:59:58 -0500

    update

--- FastClicker/src/FastClicker/GameCode.java
+++ FastClicker/src/FastClicker/GameCode.java
@@ -10,23 +10,27 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
 public class GameCode extends Application  {
-
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("FastClicker");
+    
+	private static final int _WIDTH = 250;
+	private static final int _HEIGHT = 200;
+	
+	
+    public void start(Stage primaryStage) throws Exception 
+    {
+    	primaryStage.setTitle("FastClicker");
 
         Button button = new Button("Click Me");
-        button.setStyle("-fx-font-size: 2em; -fx-padding: 50; -fx-background-color:#ffffff ");
+        button.setStyle (_WIDTH - 67,_HEIGHT-43,65,40,"OK", -fx-background-color:#ffffff , new Action()
+        		//("-fx-font-size: 2em; -fx-padding: 50; -fx-background-color:#ffffff ")
         
         HBox hbox = new HBox(button);
 
         Scene scene = new Scene(hbox, 700, 500);
         primaryStage.setScene(scene);
         primaryStage.show();
+   }
 
-    }
 
-    public static void main(String[] args) {
-        Application.launch(args);
-    }
-}
+
+	}
